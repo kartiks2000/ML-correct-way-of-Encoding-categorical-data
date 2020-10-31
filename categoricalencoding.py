9a0e8c727ccda6ef47a8d2cda9dfce303e716598
kartiks2000 <46376692+kartiks2000@users.noreply.github.com>
2020-10-31 15:21:13 +0530

    Update categoricalencoding.py

--- categoricalencoding.py
+++ categoricalencoding.py
@@ -1,8 +1,17 @@
 
+# Encoding categorical data and also implementing dummy vairiable
 
-# Encoding categorical data
+# Encoding the Independent Variable
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
-ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [3])], remainder='passthrough')
-X = np.array(ct.fit_transform(X))
+# ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), ["Enter the index of coumn u want to incode"])], remainder='passthrough')
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])], remainder='passthrough')
+x = np.array(ct.fit_transform(x))
 print(X)
+
+
+# Encoding the Dependent Variable
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+y = le.fit_transform(y)
+print(y)
