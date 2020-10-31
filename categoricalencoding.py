8670c1d6d7c69653ae5f211b46d9dd8d9e269a0c
kartiks2000 <46376692+kartiks2000@users.noreply.github.com>
2020-10-31 11:14:18 +0530

    Create categoricalencoding.py

---
+++ categoricalencoding.py
@@ -0,0 +1,8 @@
+
+
+# Encoding categorical data
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [3])], remainder='passthrough')
+X = np.array(ct.fit_transform(X))
+print(X)
